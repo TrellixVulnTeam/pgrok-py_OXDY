68a51b9f20df476cbdd0d37d1c0fac21b2b9e3c5
Sandip Dey <sandip.dey1988@yahoo.com>
2021-08-27 01:47:11 +0100

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 
-if __name__ == "__main__":
-    setup(
-        name="pgrok-py",
-        entry_points={"console_scripts": ["pgrokpy=pgrok.pgrok:main"]},
-        version="0.1.0",
-        description="Python client for interacting with Pgrok!",
-        long_description=open("README.md").read(),
-        long_description_content_type="text/markdown",
-        author="Sandip Dey",
-        author_email="sandip.dey1988@yahoo.com",
-        url="https://github.com/sandyz1000/pgrok-py",
-        license="Apache License",
-        packages=find_packages(include=['pgrok']),
-        include_package_data=True,
-        install_requires=["psutils", "PyYAML"],
-        platforms=["linux", "unix"],
-        python_requires=">3.5.2",
-    )
+setup(
+    name="pgrok",
+    packages=["pgrok"],
+    include_package_data=True,
+    install_requires=["psutils", "PyYAML"],
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["pgrokpy=pgrok.pgrok:main"]},
+    version="0.1.0",
+    description="Python client for interacting with Pgrok!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sandip Dey",
+    author_email="sandip.dey1988@yahoo.com",
+    url="https://github.com/sandyz1000/pgrok-py",
+    license="Apache License"
+)
