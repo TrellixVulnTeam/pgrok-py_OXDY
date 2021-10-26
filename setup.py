f3b1285dbd289bc9fb5c7132b2dd540c7305d53f
Sandip Dey <sandip.dey1988@yahoo.com>
2021-10-26 13:21:44 +0100

    bug fix setpy

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
 
 
 setup(
     name="pgrok",
-    packages=["pgrok"],
-    include_package_data=True,
-    install_requires=["PyYAML"],
-    python_requires=">=3.5",
-    entry_points={"console_scripts": ["pgrokpy=pgrok.pgrok:main"]},
     version="0.1.0",
+    entry_points={"console_scripts": ["pgrokpy=pgrok.pgrok:main"]},
     description="Python client for interacting with Pgrok!",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Sandip Dey",
+    include_package_data=True,
+    packages=["pgrok"],
     author_email="sandip.dey1988@yahoo.com",
     keywords=['ssh-tunnell', 'unix-tools', ''],
     url="https://github.com/sandyz1000/pgrok-py",
     license="Apache License",
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache License',
-        'Programming Language :: Python :: 3.7+',
-    ],
+    install_requires=["PyYAML"],
+    platforms=["linux", "unix"],
+    python_requires=">=3.5",
 )
